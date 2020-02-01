b8d2f90dc9007a4ef19e5eac89fc9fa3f7dcbd46
leehyukje <lims2733@naver.com>
2020-02-01 15:52:23 +0900

    컨트롤러 및 dto 테스트 코드 작성

---
+++ src/test/java/com/hyukje/book/springboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,25 @@
+package com.hyukje.book.springboot.web.dto;
+
+
+import com.hyukje.book.springboot.dto.HelloResponseDto;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void lombok_test(){
+
+        // 주어진 변수
+        String name = "test";
+        int amount = 1000;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name,amount);
+
+        //then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
